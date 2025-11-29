f573f9078b9b134bc676142539fa6c0f5ec5bd88
Harsh Sharma <harssh.ssarma@gmail.com>
2025-11-30 04:30:54 +0530

    new

--- backend/fastapi/engine/__init__.py
+++ backend/fastapi/engine/__init__.py
@@ -3,6 +3,7 @@ from .hardware_detector import HardwareDetector, get_hardware_profile
 from .adaptive_executor import AdaptiveExecutor, get_adaptive_executor
 from .stage1_clustering import LouvainClusterer
 from .stage2_cpsat import AdaptiveCPSATSolver
+from .stage2_greedy import SmartGreedyScheduler
 from .stage2_ga import GeneticAlgorithmOptimizer
 from .stage3_rl import RLConflictResolver, ContextAwareRLAgent
 
@@ -10,7 +11,7 @@ __all__ = [
     'HardwareDetector', 'get_hardware_profile',
     'AdaptiveExecutor', 'get_adaptive_executor',
     'LouvainClusterer',
-    'AdaptiveCPSATSolver',
+    'AdaptiveCPSATSolver', 'SmartGreedyScheduler',
     'GeneticAlgorithmOptimizer',
     'RLConflictResolver', 'ContextAwareRLAgent'
 ]
